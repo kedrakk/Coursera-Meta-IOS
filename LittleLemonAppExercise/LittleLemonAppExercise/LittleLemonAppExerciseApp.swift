befd264b04535a186f5b8a695d54ddd449ddf18b
kedrakk <keaindrak@gmail.com>
2023-07-11 14:38:09 +0630

    added controls and images

---
+++ LittleLemonAppExercise/LittleLemonAppExercise/LittleLemonAppExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppExerciseApp.swift
+//  LittleLemonAppExercise
+//
+//  Created by kedk on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonAppExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
