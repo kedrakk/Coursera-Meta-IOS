a70fc115a73b86389a070fa5623a79cc6870cb23
kedrakk <keaindrak@gmail.com>
2023-07-17 21:33:33 +0630

    wrap up

---
+++ littlelemonreservation/littlelemonreservation/littlelemonreservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonReservationApp.swift
+//  LittleLemonReservation
+//
+//  Created by 葉尼爾 on 2023/7/17.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
