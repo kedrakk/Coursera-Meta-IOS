8bf8dda1cb92d77155010fe63f6da44f8b1718b0
kedrakk <keaindrak@gmail.com>
2023-07-29 22:15:55 +0630

    error catching playground

---
+++ AdvancedCoursePlayGround.playground/Pages/ErrorCatchingPlayground.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+import Foundation
+
+enum CalculatorError: Error{
+    case unableToDivideByZero
+}
+
+class Calculator
+{
+    func divide(x:Double,y:Double) throws ->Double
+    {
+        if y == 0{
+            throw CalculatorError.unableToDivideByZero
+        }
+        return x/y
+    }
+}
+
+let calculator = Calculator()
+do{
+    defer{
+        print("Hello World")
+    }
+    let successResult = try calculator.divide(x:8.0,y:3.0)
+    print("\(successResult)")
+}
+catch CalculatorError.unableToDivideByZero{
+    print("Division by zero detected and not allowed")
+}
